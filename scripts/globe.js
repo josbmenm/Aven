86ccb3fced8effc285bdee14c3876bdc1afe6048
Eric Vicenti <eric@aven.io>
2018-07-23 00:27:42 -0700

    globe command should build for client repos

--- scripts/globe.js
+++ scripts/globe.js
@@ -53,11 +53,26 @@ async function start(args) {
   });
 }
 
+const localGlobeDir = pathJoin(__dirname, '..');
+
 async function build(args) {
   const appName = args[0];
-}
 
-async function runProd() {
+  console.log('Building app ' + appName);
+  let globeDir = process.env.GLOBE_DIR;
+  if (!globeDir) {
+    globeDir = pathJoin(process.env.HOME, `.globe/g-${Date.now()}-globe`);
+    console.log(
+      'No GLOBE_DIR found in the environment. Creating a new globe working directory: ' +
+        globeDir,
+    );
+    await fs.mkdirp(globeDir);
+    await spawn(
+      'rsync',
+      ['-a', '--exclude', 'node_modules', localGlobeDir + '/', globeDir],
+      { stdio: 'inherit' },
+    );
+  }
   await spawn(
     'rsync',
     [
@@ -67,11 +82,23 @@ async function runProd() {
     ],
     { stdio: 'inherit' },
   );
+  await spawn('yarn', [], { stdio: 'inherit', cwd: globeDir });
+
+  await spawn('yarn', ['build', appName], {
+    stdio: 'inherit',
+    cwd: globeDir,
+  });
+
+  await spawn(
+    'rsync',
+    ['-a', pathJoin(globeDir, 'build') + '/', pathJoin(process.cwd(), 'build')],
+    { stdio: 'inherit' },
+  );
 }
 
 const command = process.argv[2];
 const args = process.argv.slice(3);
-const commandFunctions = { start, build, 'run-prod': runProd };
+const commandFunctions = { start, build };
 const mainFn = commandFunctions[command];
 
 if (!mainFn) {
