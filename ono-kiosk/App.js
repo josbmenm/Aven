89cb55943d24486a732951198a7a77957b2e8393
Eric Vicenti <eric@aven.io>
2018-08-11 16:55:30 -0700

    Basic transition progress

--- ono-kiosk/App.js
+++ ono-kiosk/App.js
@@ -7,14 +7,59 @@ import {
   Button,
   TouchableOpacity,
   Image,
+  Animated,
 } from 'react-native';
-import { createSwitchNavigator } from '../react-navigation-core';
+import { createStackNavigator } from '../react-navigation-stack';
+import { Transitioner } from '../react-navigation-transitioner';
 
 const colors = {
   bg: '#FFF3EB',
   link: '#4987F9',
   title: '#4E6F9D',
 };
+const smoothOpenAnimator = value =>
+  new Promise(resolve => {
+    Animated.timing(value, {
+      duration: 300,
+      toValue: 1,
+    }).start(resolve);
+  });
+const smoothCloseAnimator = value =>
+  new Promise(resolve => {
+    Animated.timing(value, {
+      duration: 300,
+      toValue: 0,
+    }).start(resolve);
+  });
+
+class FadeInTransition extends React.Component {
+  static navigationOptions = {
+    openAnimator: smoothOpenAnimator,
+    closeAnimator: smoothCloseAnimator,
+  };
+  render() {
+    const { progress, navigation, children } = this.props;
+    return (
+      <Animated.View
+        style={{
+          flex: 1,
+          opacity: progress,
+          backgroundColor: 'white',
+          transform: [
+            {
+              translateY: progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [100, 0],
+              }),
+            },
+          ],
+        }}
+      >
+        {children}
+      </Animated.View>
+    );
+  }
+}
 
 const LogoText = () => (
   <Text
@@ -29,7 +74,79 @@ const LogoText = () => (
   </Text>
 );
 
-const Main = () => <LogoText />;
+class Home extends React.Component {
+  static navigationOptions = FadeInTransition.navigationOptions;
+  render() {
+    const { navigation } = this.props;
+    return (
+      <FadeInTransition {...this.props}>
+        <LogoText />
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Product');
+          }}
+        >
+          <Text>Order Smoothie</Text>
+        </TouchableOpacity>
+      </FadeInTransition>
+    );
+  }
+}
+
+class Product extends React.Component {
+  static navigationOptions = FadeInTransition.navigationOptions;
+  render() {
+    const { navigation } = this.props;
+    return (
+      <FadeInTransition {...this.props}>
+        <Text
+          style={{
+            fontSize: 64,
+            color: colors.title,
+            fontFamily: 'Shrikhand',
+            marginVertical: 25,
+          }}
+        >
+          product
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Text>go back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Order');
+          }}
+        >
+          <Text>Check out</Text>
+        </TouchableOpacity>
+      </FadeInTransition>
+    );
+  }
+}
+
+class Order extends React.Component {
+  static navigationOptions = FadeInTransition.navigationOptions;
+  render() {
+    return (
+      <FadeInTransition {...this.props}>
+        <Text
+          style={{
+            fontSize: 64,
+            color: colors.title,
+            fontFamily: 'Shrikhand',
+            marginVertical: 25,
+          }}
+        >
+          order
+        </Text>
+      </FadeInTransition>
+    );
+  }
+}
 
 const fontsCSS = `
 @font-face {
@@ -42,14 +159,22 @@ const fontsCSS = `
 }
 `;
 
-Main.navigationOptions = {
-  backgroundColor: colors.bg,
-  title: 'Kiosk',
-  customCSS: fontsCSS,
-};
-
-const App = createSwitchNavigator({
-  Main,
-});
+const App = Transitioner(
+  {
+    Home: {
+      screen: Home,
+      path: '',
+    },
+    Product,
+    Order,
+  },
+  {
+    navigationOptions: {
+      title: 'ono kiosk',
+      backgroundColor: colors.bg,
+      customCSS: fontsCSS,
+    },
+  },
+);
 
 export default App;
