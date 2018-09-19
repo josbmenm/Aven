6dfa9fb4b06a3fcf5e3561dd52f087d3fb1726aa
Eric Vicenti <eric@aven.io>
2018-09-19 12:32:58 -0700

    Upgrade deploy infra for new dependency

--- ono-cloud/MainServer.js
+++ ono-cloud/MainServer.js
@@ -1,6 +1,9 @@
 import App from './App';
 import WebServer from '../infra/WebServer';
 
+var Airtable = require('airtable');
+// var base = new Airtable({apiKey: 'YOUR_API_KEY'}).base('app0LDIj57JINdgFU');
+
 const runServer = () => WebServer(App);
 
 export default runServer;
