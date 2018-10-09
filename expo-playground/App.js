593c8bb459f03edf78ea35caab6562ddba8d6a7e
Eric Vicenti <eric@aven.io>
2018-10-08 19:01:19 -0700

    basic globe env working

---
+++ expo-playground/App.js
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = ({}) => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <Text
+        onPress={() => {
+          alert("hello");
+        }}
+      >
+        Test me!
+      </Text>
+    </View>
+  );
+};
+
+export default App;
