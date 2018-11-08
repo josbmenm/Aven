5bbf80d4624b7e216f385829e481ecc73471447c
Eric Vicenti <eric@aven.io>
2018-11-08 15:55:06 -0800

    fixfix

--- aven-cloud-postgres/startPostgresDataSource.js
+++ aven-cloud-postgres/startPostgresDataSource.js
@@ -1,6 +1,6 @@
 import { Observable, BehaviorSubject } from "rxjs-compat";
 
-const { schema, getSqlType } = require("../aven-cloud-server/schema");
+const { schema, getSqlType } = require("./schema");
 const crypto = require("crypto");
 const stringify = require("json-stable-stringify");
 
