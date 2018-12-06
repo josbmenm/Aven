a894efd222033c79cd86adf1bd737e579502a206
Eric Vicenti <eric@aven.io>
2018-12-06 13:54:41 -0800

    😿 forgotten vendor import file added

---
+++ aven-tools/importVendorCode.js
@@ -0,0 +1,162 @@
+const fs = require('fs-extra');
+const pathJoin = require('path').join;
+const pathRelative = require('path').relative;
+const babel = require('babel-core');
+
+const srcRoot = pathJoin(__dirname, '..');
+
+function runTransform(input, vendedSrcDeps, fileDest) {
+  const handledBabel = babel.transform(input, {
+    sourceMaps: true,
+    comments: true,
+    parserOpts: {
+      plugins: ['jsx', 'objectRestSpread', 'classProperties', 'flow'],
+    },
+    plugins: [
+      ({ parse, traverse }) => ({
+        visitor: {
+          ImportDeclaration(path) {
+            const sourceName = path.node.source.value;
+            const vendedDep = vendedSrcDeps[sourceName];
+            if (vendedDep) {
+              const destModule = pathJoin(srcRoot, vendedDep);
+              const fileDestDir = pathJoin(fileDest, '..');
+              path.node.source.value = pathRelative(fileDestDir, destModule);
+            }
+          },
+        },
+      }),
+    ],
+  });
+  return handledBabel.code;
+}
+
+async function copyWithTransform(source, dest, vendedSrcDeps, file = '') {
+  const fileSource = pathJoin(source, file);
+  const fileDest = pathJoin(dest, file);
+  const stat = await fs.stat(fileSource);
+  if (stat.isDirectory()) {
+    const files = await fs.readdir(fileSource);
+    await fs.mkdirp(fileDest);
+    await Promise.all(
+      files.map(innerFile =>
+        copyWithTransform(
+          source,
+          dest,
+          vendedSrcDeps,
+          pathJoin(file, innerFile),
+        ),
+      ),
+    );
+  } else {
+    if (file.match(/\.DS_Store$/)) {
+      return;
+    }
+    if (!file.match(/\.js$/)) {
+      await fs.copy(fileSource, fileDest);
+      return;
+    }
+    const sourceCode = await fs.readFile(fileSource);
+    const transformedCode = runTransform(sourceCode, vendedSrcDeps, fileDest);
+    await fs.writeFile(fileDest, transformedCode);
+  }
+}
+
+async function vendUpstream(
+  { moduleName, srcName, moduleSrcPath },
+  vendedSrcDeps,
+) {
+  const destPath = pathJoin(srcRoot, srcName);
+  const fullModuleSrcPath = pathJoin(
+    srcRoot,
+    'node_modules',
+    moduleName,
+    moduleSrcPath,
+  );
+  const srcRootPkg = JSON.parse(
+    await fs.readFile(pathJoin(srcRoot, 'package.json')),
+  );
+  const modulePkgPath = pathJoin(
+    srcRoot,
+    'node_modules',
+    moduleName,
+    'package.json',
+  );
+  const srcPkg = JSON.parse(await fs.readFile(modulePkgPath));
+  const peerDeps = new Set(Object.keys(srcPkg.peerDependencies || {}));
+  const moduleDependencies = Object.keys(srcPkg.dependencies || {});
+  const srcDependencies = [];
+  const destPkg = {
+    name: moduleName,
+    aven: { srcDependencies, moduleDependencies },
+  };
+  moduleDependencies.forEach(depName => {
+    const depVersion = srcPkg.dependencies[depName];
+    const rootDepVersion = srcRootPkg.dependencies[depName];
+    if (!rootDepVersion) {
+      throw new Error(
+        `Cannot find dependency ${depName} in root package! Expecting version ${depVersion}`,
+      );
+      // console.warn(`yarn add ${depName}@${depVersion}`);
+    }
+    if (rootDepVersion !== depVersion) {
+      console.warn(
+        `Version mismatch of ${depName}. Expecting ${depVersion}, observed ${rootDepVersion}`,
+      );
+    }
+  });
+
+  await copyWithTransform(fullModuleSrcPath, destPath, vendedSrcDeps);
+
+  await fs.writeFile(
+    pathJoin(destPath, 'package.json'),
+    JSON.stringify(destPkg, null, 2),
+  );
+}
+
+async function vendUpstreams(upstreams) {
+  const vendedSrcDeps = {};
+  upstreams.map(u => {
+    vendedSrcDeps[u.moduleName] = u.srcName;
+  });
+  console.log(`🗄  Copying the following modules from your dependencies into this workspace:
+
+${upstreams
+    .map(u => `node_modules/${u.moduleName} -> ./${u.srcName}`)
+    .join('\n')}
+
+⚠️  - You are responsible for syncronizing workspace source with the original modules!`);
+  await Promise.all(upstreams.map(u => vendUpstream(u, vendedSrcDeps)));
+}
+
+vendUpstreams([
+  {
+    moduleName: '@react-navigation/core',
+    srcName: 'navigation-core',
+    moduleSrcPath: 'src',
+  },
+  {
+    moduleName: '@react-navigation/web',
+    srcName: 'navigation-web',
+    moduleSrcPath: 'src',
+  },
+  {
+    moduleName: '@react-navigation/native',
+    srcName: 'navigation-native',
+    moduleSrcPath: 'src',
+  },
+  {
+    moduleName: 'react-navigation-hooks',
+    srcName: 'navigation-hooks',
+    moduleSrcPath: 'src',
+  },
+  {
+    moduleName: 'react-navigation-stack',
+    srcName: 'navigation-stack',
+    moduleSrcPath: 'src',
+  },
+])
+  .then(() => {
+    console.log('✅ Done!');
+  })
+  .catch(console.error);
