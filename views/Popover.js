587e06e63b0baa7d505a4916883e39050f4ea5df
Eric Vicenti <eric@aven.io>
2019-03-18 08:44:57 -0700

    🎈 popover work

--- views/Popover.js
+++ views/Popover.js
@@ -4,52 +4,54 @@ import Animated, { Easing } from 'react-native-reanimated';
 
 const PopoverContext = React.createContext(null);
 
-export function PopoverTarget({ renderContent, renderPopover }) {
+export function PopoverTarget({ renderContent, renderPopover, timing }) {
   const ctx = useContext(PopoverContext);
   async function onPopover() {
-    console.log('woah pop');
     const location = await new Promise(resolve =>
       viewRef.current.measure((x, y, width, height, pageX, pageY) => {
-        console.log('hiho', location);
         resolve({ x, y, width, height, pageX, pageY });
       }),
     );
-    ctx.openPopover(renderPopover(), location);
+    ctx.openPopover(renderPopover(), location, timing);
   }
   const viewRef = React.createRef();
   return <View ref={viewRef}>{renderContent(onPopover)}</View>;
 }
 
-export function useTargetPopover(renderPopover) {
+export function useTargetPopover(renderPopover, timing) {
   const ctx = useContext(PopoverContext);
   const targetRef = React.createRef();
   async function onPopover() {
-    console.log('TARGETED pop');
     const location = await new Promise(resolve =>
       targetRef.current.measure((x, y, width, height, pageX, pageY) => {
-        console.log('hiho', location);
         resolve({ x, y, width, height, pageX, pageY });
       }),
     );
-    ctx.openPopover(renderPopover, location);
+    ctx.openPopover(renderPopover, location, timing);
   }
   return { targetRef, onPopover };
 }
 
+const defaultTiming = {
+  duration: 1000,
+  easing: Easing.linear,
+};
+
 export function PopoverContainer({ children }) {
+  let [configuredTiming, setConfiguredTiming] = useState(null);
   let [popoverOpenValue] = useState(new Animated.Value(0));
   let [popover, setPopover] = useState(null);
   let [containerLayout, setContainerLayout] = useState(null);
   function closePopover() {
     Animated.timing(popoverOpenValue, {
-      toValue: 0,
-      duration: 500,
-      easing: Easing.linear,
+      toValue: new Animated.Value(0),
+      ...(configuredTiming || defaultTiming),
     }).start(() => {
       setPopover(null);
     });
   }
-  function openPopover(renderPopover, location) {
+  function openPopover(renderPopover, location, timing) {
+    setConfiguredTiming(timing);
     setPopover(
       renderPopover({
         onClose: closePopover,
@@ -59,9 +61,8 @@ export function PopoverContainer({ children }) {
       }),
     );
     Animated.timing(popoverOpenValue, {
-      toValue: 1,
-      duration: 500,
-      easing: Easing.linear,
+      toValue: new Animated.Value(1),
+      ...(timing || defaultTiming),
     }).start(() => {});
   }
 
