dafb574e1f927d902af3b1e932bedae959c324cd
Eric Vicenti <eric@aven.io>
2019-04-06 11:47:04 +0200

    📜 touch quick start

--- aven-io/docs/QuickStart.js
+++ aven-io/docs/QuickStart.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Title, Body, Page, List, ListItem, Link } from '../DocViews';
+import { Title, Body, Page, List, Snippet, ListItem, Link } from '../DocViews';
 
 function DocPage() {
   return (
     <Page>
       <Title>Quick Start</Title>
       <Body>
-        The easiest way to start with Aven Cloud is to clone one of the sample
-        repositories:
+        The easiest way to start with Aven Cloud is to clone the main repo and
+        run the web and native apps:
       </Body>
-      <List>
-        <ListItem>
-          <Link url="https://github.com/AvenCloud/Aven-Example-Isomorphic">
-            Isomorphic Example
-          </Link>
-        </ListItem>
-        <ListItem>Client-Only Example</ListItem>
-        <ListItem>Server-Only Example</ListItem>
-      </List>
+      <Snippet
+        code={`
+git clone git@github.com:AvenCloud/Aven.git
+cd Aven
+yarn
+yarn start todo-web
+# wait for app to start. In seperate tab, run the RN server:
+yarn start todo-native
+`}
+      />
+      <Body>
+        To get started, set up the data{' '}
+        <Link routeName="Tutorial1">sources for your app</Link>.{' '}
+      </Body>
     </Page>
   );
 }
