3bc7cf1a72e8e9159d7b8682ec12dd5ec1c2b1e3
Eric Vicenti <eric@aven.io>
2019-04-09 09:20:29 -0700

    🐘 pg prod knex config for migration script

--- cloud-postgres/knexfile.js
+++ cloud-postgres/knexfile.js
@@ -1,7 +1,21 @@
+function getEnv(n) {
+  return process.env[n];
+}
+
 module.exports = {
   testing: {
     client: 'pg',
     connection:
       'postgresql://postgres:aven-test-password@localhost:5432/postgres',
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      ssl: !!getEnv('SQL_USE_SSL'),
+      user: getEnv('SQL_USER'),
+      password: getEnv('SQL_PASSWORD'),
+      database: getEnv('SQL_DATABASE'),
+      host: getEnv('SQL_HOST'),
+    },
   },
 };
