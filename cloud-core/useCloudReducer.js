f9babd0adca9771a271cb3875addd71e5f726d9b
Eric Vicenti <eric@aven.io>
2019-04-23 06:38:21 -0700

    🗂 split out defineCloudReducer

--- cloud-core/useCloudReducer.js
+++ cloud-core/useCloudReducer.js
@@ -1,29 +1,20 @@
 import useCloud from './useCloud';
 import useObservable from './useObservable';
-import defineCloudFunction from './defineCloudFunction';
 
-export function defineCloudReducer(reducerName, reducerFn, initialState) {
-  const fn = ({ value, id }, doc, cloud, useValue) => {
-    let state = initialState;
-    if (value === undefined || value === null) {
-      return state;
-    }
-    let action = value.value;
-    if (value.on && value.on.id) {
-      const ancestorName = `${doc.getFullName()}#${value.on.id}^${reducerName}`;
-      state = useValue(cloud.get(ancestorName));
-    }
-    return reducerFn(state, action);
-  };
-  return defineCloudFunction(reducerName, fn);
-}
-
-export default function useCloudReducer(actionDocName, cloudReducer) {
+export default function useCloudReducer(
+  actionDocName,
+  cloudReducer,
+  remote = true,
+) {
   const cloud = useCloud();
+  if (remote) {
+    const reducerDoc = cloud.get(cloudReducer.name);
+    reducerDoc.markRemoteLambda(true);
+  }
   const actionsDoc = cloud.get(actionDocName);
   if (cloudReducer.type !== 'CloudFunction') {
     throw new Error(
-      'Invalid reducer provided to useCloudReducer. Create it with defineCloudReducer'
+      'Invalid reducer provided to useCloudReducer. Create it with defineCloudReducer',
     );
   }
   cloud.lazyDefineCloudFunction(cloudReducer);
@@ -31,53 +22,3 @@ export default function useCloudReducer(actionDocName, cloudReducer) {
   const todos = useObservable(todosReduced.observeValue);
   return [todos, actionsDoc.putTransaction];
 }
-
-/*
-
-const cloudFn = defineCloudFunction('FnName', fn)
-const reducer = defineCloudReducer('ReducerName', reducer, {init: 'state'})
-
-useCloudValue(cloudFn)
-
-cloud.get(cloudFn)
-
-createEvalSource({
-  source,
-  functions: [cloudFn, reducer],
-})
-
-useCloudReducer('StateDocName', reducer)
-
-*/
-
-// export function createReducerLambda(reducerName, reducerFn, initialState) {
-//   return ({ value, id }, doc, cloud, useValue) => {
-//     let state = initialState;
-//     if (value === undefined || value === null) {
-//       return state;
-//     }
-//     let action = value.value;
-//     if (value.on && value.on.id) {
-//       const ancestorName = `${doc.getFullName()}#${value.on.id}^${reducerName}`;
-//       state = useValue(cloud.get(ancestorName));
-//     }
-//     return reducerFn(state, action);
-//   };
-// }
-
-// export default function useCloudReducer(
-//   actionDocName,
-//   reducerName,
-//   reducerFn,
-//   initialState
-// ) {
-//   const cloud = useCloud();
-//   const actionsDoc = cloud.get(actionDocName);
-//   cloud.setLambda(
-//     reducerName,
-//     createReducerLambda(reducerName, reducerFn, initialState)
-//   );
-//   const todosReduced = actionsDoc.get(`^${reducerName}`);
-//   const todos = useObservable(todosReduced.observeValue);
-//   return [todos, actionsDoc.putTransaction];
-// }
