cd245840d1fdfd1ea4e55f6c8ea98d8686d70131
Eric Vicenti <eric@aven.io>
2019-05-05 12:03:56 -0700

    🔒 memory stream connectivity

--- cloud-core/createMemoryStorageSource.js
+++ cloud-core/createMemoryStorageSource.js
@@ -1,6 +1,7 @@
 import { BehaviorSubject } from 'rxjs-compat';
 import createGenericDataSource from './createGenericDataSource';
 import getIdOfValue from '../cloud-utils/getIdOfValue';
+import xs from 'xstream';
 
 export default function createMemoryStorageSource(opts = {}) {
   const primaryDomain = opts.domain;
@@ -16,6 +17,7 @@ export default function createMemoryStorageSource(opts = {}) {
   const docState = { children: {}, childrenSet: new Set() };
 
   const isConnected = new BehaviorSubject(true);
+  const isConnectedStream = xs.of(true);
 
   async function getBlock(blockId) {
     if (typeof blockId !== 'string') {
@@ -68,5 +70,6 @@ export default function createMemoryStorageSource(opts = {}) {
     commitDocMove,
 
     isConnected,
+    isConnectedStream,
   });
 }
