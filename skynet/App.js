9a9a16ca2ad62565e29acfb843277814bba6c3ab
Eric Vicenti <eric@aven.io>
2019-05-07 12:53:48 -0700

    clean up web presence

--- skynet/App.js
+++ skynet/App.js
@@ -10,39 +10,7 @@ import {
 } from 'react-native';
 import { createSwitchNavigator } from '../navigation-core';
 import Admin from '../admin/Admin';
-
-const colors = {
-  bg: '#FFF3EB',
-  link: '#4987F9',
-  title: '#4E6F9D',
-};
-
-const LogoText = () => (
-  <Text
-    style={{
-      fontSize: 64,
-      color: colors.title,
-      fontFamily: 'Shrikhand',
-      marginVertical: 25,
-    }}
-  >
-    ono
-  </Text>
-);
-
-const EmailLink = ({ children, to }) => (
-  <TouchableOpacity
-    onPress={() => {
-      window.location = `mailto:${to}`;
-    }}
-  >
-    <Text
-      style={{ fontSize: 24, color: colors.link, fontFamily: 'ArbutusSlab' }}
-    >
-      {children}
-    </Text>
-  </TouchableOpacity>
-);
+import { monsterra } from '../components/Styles';
 
 const Main = () => (
   <View
@@ -53,84 +21,22 @@ const Main = () => (
       flexDirection: 'row',
     }}
   >
-    <View
+    <Text
       style={{
-        ...StyleSheet.absoluteFillObject,
-        left: 0,
-        right: -375,
-        top: -50,
-        bottom: -50,
+        fontSize: 24,
+        color: monsterra,
+        fontFamily: 'Maax',
       }}
     >
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-        }}
-      >
-        <Image
-          style={{
-            flex: 1,
-            resizeMode: 'cover',
-            minWidth: 1000,
-            minHeight: 950,
-          }}
-          source={require('./graphics/ono-shapes.svg')}
-        />
-      </View>
-    </View>
-    <View
-      style={{
-        maxWidth: 500,
-        flex: 1,
-        paddingTop: 50,
-        paddingBottom: 200,
-        paddingHorizontal: 30,
-        justifyContent: 'center',
-      }}
-    >
-      <LogoText />
-      <View
-        style={{
-          flexWrap: 'wrap',
-          flexDirection: 'row',
-          marginVertical: 20,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 24,
-            color: colors.title,
-            fontFamily: 'ArbutusSlab',
-          }}
-        >
-          dine &amp; dash /{' '}
-        </Text>
-        <Text
-          style={{
-            fontSize: 24,
-            color: colors.title,
-            fontFamily: 'ArbutusSlab',
-          }}
-        >
-          coming 2019
-        </Text>
-      </View>
-      <EmailLink to="aloha@onofood.co">say aloha →</EmailLink>
-      <View style={{ flex: 1, maxHeight: '50%' }} />
-    </View>
+      not found
+    </Text>
   </View>
 );
 
 const fontsCSS = `
 @font-face {
-  src: url('/fonts/Shrikhand.otf');
-  font-family: Shrikhand;
-}
-@font-face {
-  src: url('/fonts/ArbutusSlab.ttf');
-  font-family: ArbutusSlab;
+  src: url('/fonts/Maax - Bold-205TF/Maax - Bold-205TF.ttf');
+  font-family: Maax;
 }
 `;
 
@@ -166,21 +72,24 @@ function SkynetAdmin(props) {
 }
 SkynetAdmin.router = Admin.router;
 
-const App = createSwitchNavigator({
-  Main: {
-    screen: Main,
-    navigationOptions: {
-      backgroundColor: colors.bg,
-      title: 'ono food co',
-      customCSS: fontsCSS,
-      customHTML: GoogleAnalyticsTag,
+const App = createSwitchNavigator(
+  {
+    Main: {
+      screen: Main,
+      navigationOptions: {
+        backgroundColor: '#FFFFFF',
+        title: 'ono food co',
+        customCSS: fontsCSS,
+        customHTML: GoogleAnalyticsTag,
+      },
+      path: '',
     },
-    path: '',
+    Admin: {
+      screen: SkynetAdmin,
+      path: 'admin',
+    },
   },
-  Admin: {
-    screen: SkynetAdmin,
-    path: 'admin',
-  },
-});
+  {},
+);
 
 export default App;
