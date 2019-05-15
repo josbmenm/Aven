feea1a85c0e9a427e203b8bef7df6f8ce1b96e6c
Eric Vicenti <eric@aven.io>
2019-05-14 19:21:32 -0700

    🤦‍♀️ Disable broken HMR

--- aven-tools/proto/web/src/index.js
+++ aven-tools/proto/web/src/index.js
@@ -6,13 +6,12 @@ server().then(instance => {
   serverInstance = instance;
 });
 
-if (module.hot) {
-  console.log('✅  Server-side HMR Enabled!');
-
-  module.hot.accept('./server', async () => {
-    console.log('🔁  HMR Reloading `./server`...');
-    await serverInstance.close();
-    const newServer = require('./server').default;
-    serverInstance = await newServer();
-  });
-}
+// if (module.hot) {
+//   console.log('✅  Server-side HMR Enabled!');
+//   module.hot.accept('./server', async () => {
+//     console.log('🔁  HMR Reloading `./server`...');
+//     await serverInstance.close();
+//     const newServer = require('./server').default;
+//     serverInstance = await newServer();
+//   });
+// }
