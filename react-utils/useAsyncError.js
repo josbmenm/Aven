b0bc0fecd570c8095c02bf774dc6e138b34724da
Eric Vicenti <eric@aven.io>
2019-05-29 00:21:19 -0700

    🚔 Async error handler

--- react-utils/useAsyncError.js
+++ react-utils/useAsyncError.js
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 
-export default function useAsyncError() {
+export default function useAsyncError(onError) {
   const [error, setError] = useState(null);
   if (error) {
     throw error;
   }
   function handler(promise) {
     return promise.catch(error => {
-      setError(error);
+      const handled = onError && onError(error);
+      if (!handled) {
+        setError(error);
+      }
     });
   }
   return handler;
