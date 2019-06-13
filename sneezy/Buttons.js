e518f5c998227d1900515b9489202a61a9e22e08
Horacio Herrera <me@hherrerag.com>
2019-06-13 13:59:52 +0200

    refactor buttons to use the theme

--- sneezy/Buttons.js
+++ sneezy/Buttons.js
@@ -1,36 +1,107 @@
-import React from "react";
-import { View, Text } from "react-native-web";
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { ThemeContext } from './ThemeContext';
 
-export function Button({ style, text = "button", onPress, ...rest }) {
+const defaultButtonStyles = {
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontFamily: 'Maax-Bold',
+    fontSize: 20,
+  },
+};
+
+export function Button({
+  buttonStyle,
+  text = 'button',
+  onPress,
+  type = 'solid', // solid | outline
+  variant = 'primary', // primary | secondary
+  textStyle = {},
+  ...rest
+}) {
+  const [focusStyles, setFocusStyles] = React.useState({});
+  const { colors } = React.useContext(ThemeContext);
+
+  function onFocus() {
+    setFocusStyles({
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+
+      elevation: 5,
+    });
+  }
+
+  function onBlur() {
+    setFocusStyles({});
+  }
+
   return (
-    <View
-      style={{
-        paddingHorizontal: 20,
-        paddingVertical: 12,
-        borderRadius: 4,
-        borderWidth: 3,
-        borderColor: "#005151",
-        ...style
-      }}
-    >
-      <Text style={{ color: "#005151" }}>{text}</Text>
-    </View>
+    <TouchableOpacity onPress={onPress} onFocus={onFocus} onBlur={onBlur}>
+      <View
+        style={{
+          ...defaultButtonStyles.button,
+          borderRadius: 4,
+          borderWidth: 3,
+          borderColor: colors.primary,
+          backgroundColor: type === 'solid' ? colors.primary : 'transparent',
+          ...buttonStyle,
+          ...focusStyles,
+          cursor: 'pointer',
+        }}
+      >
+        <Text
+          style={{
+            ...defaultButtonStyles.text,
+            color: type === 'solid' ? colors.white : colors.primary,
+            ...textStyle,
+          }}
+        >
+          {text}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
-export function Link({ style, active = false, children, onPress, ...rest }) {
+export function Link({
+  buttonStyle = {},
+  textStyle = {},
+  active = false,
+  children,
+  onPress,
+  ...rest
+}) {
+  const { colors } = React.useContext(ThemeContext);
+
   return (
-    <View
-      style={{
-        paddingHorizontal: 20,
-        paddingVertical: 12,
-        borderRadius: 4,
-        borderBottomWidth: 3,
-        borderColor: active ? "#005151" : "transparent",
-        ...style
-      }}
-    >
-      <Text style={{ color: "#005151" }}>{children}</Text>
-    </View>
+    <TouchableOpacity onPress={onPress}>
+      <View
+        style={{
+          ...defaultButtonStyles.button,
+          borderBottomWidth: 3,
+          borderColor: active ? colors.primary : 'transparent',
+          ...buttonStyle,
+        }}
+      >
+        <Text
+          style={{
+            ...defaultButtonStyles.text,
+            color: colors.primary,
+            ...textStyle,
+          }}
+        >
+          {children}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 }
