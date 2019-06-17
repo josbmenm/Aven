0b6fd0c6b193d5957aa6b7c501052e2419bc2d3f
Horacio Herrera <me@hherrerag.com>
2019-06-17 19:19:30 +0200

    schedule decoration timeline line

--- sneezy/Schedule.js
+++ sneezy/Schedule.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { useTheme } from './ThemeContext';
 import { BodyText } from './Tokens';
 
@@ -10,47 +10,94 @@ function Schedule(props) {
       id: 1,
       time: '8:00 - 10:30 am',
       address: '420 7th Ave. Apt 301 San Francisco, CA, 94118',
+      active: true,
     },
     {
       id: 2,
       time: '10:30 am - 2:30 pm',
       address: '981 35th St, Oakland, CA 94608',
-    },
+      active: false,
+    }
   ];
 
   return (
     <View>
-      {schedule.map(item => (
-        <ScheduleItem key={item.id} item={item} />
+      {schedule.map((item, index) => (
+        <ScheduleItem key={item.id} item={item} first={index === 0} last={index === schedule.length - 1} />
       ))}
     </View>
   );
 }
 
-function ScheduleItem({ item }) {
+function ScheduleItem({ item, first, last }) {
   const theme = useTheme();
   return (
     <View
       style={{
-        borderRadius: 8,
-        borderWidth: 3,
-        borderColor: theme.colors.primary,
-        padding: 20,
-        marginBottom: 20,
-        ...theme.shadows.medium,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingBottom: 20,
+        position: 'relative',
       }}
     >
-      <BodyText
-        bold
+      <View
         style={{
-          fontSize: 13,
-          fontFamily: theme.fonts.title,
-          margin: 0
+          width: 3,
+          backgroundColor: theme.colors.primary,
+          position: 'absolute',
+          top: first ? '50%' : 0,
+          bottom: last ? "50%" : 0,
+          left: -40,
+        }}
+      />
+      <View
+        style={{
+          width: 20,
+          height: 20,
+          position: 'absolute',
+          left: -48,
+          backgroundColor: theme.colors.primary,
+          borderWidth: 3,
+          borderColor: theme.colors.white,
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 10,
         }}
       >
-        {item.time}
-      </BodyText>
-      <BodyText>{item.address}</BodyText>
+        {item.active && (
+          <View
+            style={{
+              backgroundColor: theme.colors.white,
+              width: 8,
+              height: 8,
+              borderRadius: 4,
+            }}
+          />
+        )}
+      </View>
+      <View
+        style={{
+          flex: 1,
+          borderRadius: 8,
+          borderWidth: 3,
+          borderColor: item.active ? theme.colors.primary : "transparent",
+          padding: 20,
+          ...theme.shadows.medium,
+        }}
+      >
+        <BodyText
+          bold
+          style={{
+            fontSize: 13,
+            lineHeight: 16,
+            fontFamily: theme.fonts.title,
+            margin: 0,
+          }}
+        >
+          {item.time}
+        </BodyText>
+        <BodyText style={{ marginBottom: 0 }}>{item.address}</BodyText>
+      </View>
     </View>
   );
 }
