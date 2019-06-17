dfd0b7da9965ef42b8d50928b46d63f3f7d4dbab
Horacio Herrera <me@hherrerag.com>
2019-06-17 14:19:21 +0200

    home carousel markup

--- sneezy/Home/HomeSlider.js
+++ sneezy/Home/HomeSlider.js
@@ -1,17 +1,48 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React from 'react';
+import { View, Image, ScrollView } from 'react-native';
 import Container from '../Container';
+
+function Slide({ source, width, height }) {
+  return (
+    <View
+      style={{
+        borderRadius: 4,
+        margin: 20,
+      }}
+    >
+      <Image source={source} style={{ width, height }} />
+    </View>
+  );
+}
 
 function HomeSlider() {
   return (
-    <View style={{
-      paddingVertical: 120,
-    }}>
+    <View
+      style={{
+        paddingVertical: 40,
+      }}
+    >
       <Container>
-        <Text>SLIDER SECTION</Text>
+        <ScrollView horizontal pagingEnabled>
+          <Slide
+            source={require('./assets/slides_1.jpg')}
+            width={292}
+            height={520}
+          />
+          <Slide
+            source={require('./assets/slides_2.jpg')}
+            width={700}
+            height={520}
+          />
+          <Slide
+            source={require('./assets/slides_3.jpg')}
+            width={292}
+            height={520}
+          />
+        </ScrollView>
       </Container>
     </View>
-  )
+  );
 }
 
 export default HomeSlider;
