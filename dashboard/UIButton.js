e194b5fda737d34d7cdcfac81653d8dccc88707e
Horacio Herrera <me@hherrerag.com>
2019-07-03 22:33:30 +0200

    🎨 fix responsive styles on buttons

--- dashboard/UIButton.js
+++ dashboard/UIButton.js
@@ -2,9 +2,10 @@ import React from 'react';
 import View from '../views/View';
 import Text from '../views/Text';
 import { useTheme } from './Theme';
+import { Responsive } from './Responsive';
 
 function UIButton({
-  buttonStyle,
+  buttonStyle = {},
   titleStyle,
   title,
   type = 'solid', // 'solid' | 'outline'
@@ -36,41 +37,21 @@ function UIButton({
       ? theme.colors.secondary80
       : theme.colors.lightGrey;
 
-  const buttonSize =
-    size === 'small'
-      ? {
-          button: {
-            paddingVertical: theme.space[1],
-            paddingHorizontal: theme.space[4],
-            borderRadius: theme.radii[1],
-          },
-          title: {
-            fontSize: theme.fontSizes[1],
-          },
-        }
-      : size === 'large'
-      ? {
-          button: {
-            paddingVertical: theme.space[4],
-            paddingHorizontal: theme.space[8],
-            borderRadius: theme.radii[3],
-          },
-          title: {
-            fontSize: theme.fontSizes[3],
-          },
-        }
-      : {
-          button: {
-            paddingVertical: theme.space[2],
-            paddingHorizontal: theme.space[6],
-            borderRadius: theme.radii[2],
-          },
-          title: { fontSize: 20 },
-        };
   return (
-    <View
-      style={[
-        {
+    <Responsive
+      style={{
+        paddingVertical:
+          buttonStyle.paddingVertical !== undefined
+            ? [buttonStyle.paddingVertical, buttonStyle.paddingVertical]
+            : [9, 15],
+        paddingHorizontal:
+          buttonStyle.paddingHorizontal !== undefined
+            ? [buttonStyle.paddingHorizontal, buttonStyle.paddingHorizontal]
+            : [24, 32],
+      }}
+    >
+      <View
+        style={{
           borderRadius: theme.radii[2],
           borderWidth: 3,
           justifyContent: 'center',
@@ -78,30 +59,29 @@ function UIButton({
           backgroundColor:
             type === 'solid' ? buttonBackgroundColor : 'transparent',
           opacity: disabled ? 0.5 : 1,
-        },
-        buttonSize.button,
-        buttonStyle,
-      ]}
-      {...rest}
-    >
-      {children ? children : null}
-      {title ? (
-        <Text
-          style={[
-            {
-              fontFamily: theme.fontFamily.button,
-              textAlign: 'center',
-              lineHeight: theme.fontSizes[2] * 1.4,
-              color: type === 'solid' ? theme.colors.white : buttonColor,
-            },
-            buttonSize.title,
-            titleStyle,
-          ]}
-        >
-          {title}
-        </Text>
-      ) : null}
-    </View>
+          ...buttonStyle,
+        }}
+        {...rest}
+      >
+        {children ? children : null}
+        {title ? (
+          <Text
+            style={[
+              {
+                fontFamily: theme.fontFamily.button,
+                fontSize: 20,
+                lineHeight: 24,
+                textAlign: 'center',
+                color: type === 'solid' ? theme.colors.white : buttonColor,
+              },
+              titleStyle,
+            ]}
+          >
+            {title}
+          </Text>
+        ) : null}
+      </View>
+    </Responsive>
   );
 }
 
