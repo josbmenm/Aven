91f4fa098a2a83a9c99cbd6bf36cc36e41055ec3
Horacio Herrera <me@hherrerag.com>
2019-07-08 10:49:16 +0200

    💥 add Title component

---
+++ dashboard/Title.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Text from '../views/Text';
+import { useTheme } from './Theme';
+import { Responsive } from './Responsive';
+
+function Title({ children, style, responsiveStyle, breakpoint, ...rest }) {
+  const theme = useTheme();
+  return (
+    <Responsive
+      breakpoint={breakpoint}
+      style={{
+        fontSize: [24, 28],
+        lineHeight: [32, 48],
+        ...responsiveStyle,
+      }}
+    >
+      <Text style={{ ...theme.textStyles.title, ...style }} {...rest}>
+        {children}
+      </Text>
+    </Responsive>
+  );
+}
+
+export default Title;
