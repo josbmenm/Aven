f371be116675f1d55055f56034b387dc4b33a814
Eric Vicenti <eric@aven.io>
2019-07-09 15:53:11 -0700

    💈 Fullscreen navigator scrolls to top

---
+++ navigation-web/createFullscreenSwitchNavigator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import createSwitchNavigator from '../navigation-core/navigators/createSwitchNavigator';
+
+export default function createFullscreenSwitchNavigator(routeConfig, options) {
+  const SwitchNavigator = createSwitchNavigator(routeConfig, options);
+
+  function FixedNavigator(props) {
+    const { state } = props.navigation;
+    const activeRoute = state.routes[state.index];
+    React.useEffect(() => {
+      global.window.scrollTo(0, 0);
+    }, [
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      JSON.stringify(activeRoute),
+    ]);
+    return <SwitchNavigator {...props} />;
+  }
+  FixedNavigator.navigationOptions = SwitchNavigator.navigationOptions;
+  FixedNavigator.router = SwitchNavigator.router;
+
+  return FixedNavigator;
+}
