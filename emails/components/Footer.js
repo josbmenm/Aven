71fc83a0ab08654a57bc3d09e9565573a9c0e69e
Horacio Herrera <me@hherrerag.com>
2019-07-11 00:02:22 +0200

    email assets + header images added

--- emails/components/Footer.js
+++ emails/components/Footer.js
@@ -1,22 +1,51 @@
-import React from 'react'
+import React from 'react';
+import theme from '../theme';
 
-import {
-  MjmlSection,
-  MjmlColumn,
-  MjmlButton,
-  MjmlImage,
-  MjmlSpacer,
-  MjmlText,
-} from 'mjml-react';
+import { MjmlSection, MjmlColumn, MjmlRaw } from 'mjml-react';
 
 function Footer() {
   return (
-    <MjmlSection>
-      <MjmlColumn>
-        <MjmlText fontSize="24px" fontWeight="bold">Footer content</MjmlText>
+    <MjmlSection
+      backgroundColor="white"
+      padding="0"
+      paddingLeft="136px"
+      paddingRight="136px"
+    >
+      <MjmlColumn padding="28px 36px 20px 36px" borderBottom="1px solid hsla(0, 0%, 59%, 0.3)">
+        <MjmlRaw>
+          <div
+            style={{
+              width: 48,
+              height: 48,
+              backgroundColor: theme.colors.primary,
+            }}
+          />
+        </MjmlRaw>
+      </MjmlColumn>
+      <MjmlColumn padding="28px 36px 20px 36px" borderBottom="1px solid hsla(0, 0%, 59%, 0.3)">
+        <MjmlRaw>
+          <div
+            style={{
+              width: 48,
+              height: 48,
+              backgroundColor: theme.colors.primary,
+            }}
+          />
+        </MjmlRaw>
+      </MjmlColumn>
+      <MjmlColumn padding="28px 36px 20px 36px" borderBottom="1px solid hsla(0, 0%, 59%, 0.3)">
+        <MjmlRaw>
+          <div
+            style={{
+              width: 48,
+              height: 48,
+              backgroundColor: theme.colors.primary,
+            }}
+          />
+        </MjmlRaw>
       </MjmlColumn>
     </MjmlSection>
-  )
+  );
 }
 
 export default Footer;
--- emails/components/Header.js
+++ emails/components/Header.js
@@ -4,21 +4,36 @@ import theme from '../theme';
 
 function Header() {
   return (
-      <MjmlSection
-        backgroundColor="white"
-        padding="0"
-        borderBottom={`1px solid ${theme.colors.primary}`}
-      >
-        <MjmlColumn />
-        <MjmlColumn padding="0">
-          <MjmlImage
-            fluidOnMobile="true"
-            width="150px"
-            src={'https://via.placeholder.com/260x132'}
-          />
-        </MjmlColumn>
-        <MjmlColumn />
-      </MjmlSection>
+    <MjmlSection
+      backgroundColor="white"
+      padding="0"
+      borderBottom={`1px solid ${theme.colors.primary}`}
+    >
+      <MjmlColumn padding="0" verticalAlign="top">
+        <MjmlImage
+          align="left"
+          padding="0"
+          width="88px"
+          height="64px"
+          src="https://emailassets-kglyyrazbw.now.sh/email_top-left-corner.png"
+        />
+      </MjmlColumn>
+      <MjmlColumn padding="0" verticalAlign="middle">
+        <MjmlImage
+          width="150px"
+          src="https://emailassets-kglyyrazbw.now.sh/email_logo.png"
+        />
+      </MjmlColumn>
+      <MjmlColumn verticalAlign="bottom" padding="0">
+        <MjmlImage
+          align="right"
+          padding="0"
+          width="104px"
+          height="66px"
+          src="https://emailassets-kglyyrazbw.now.sh/email_bottom-right-corner.jpg"
+        />
+      </MjmlColumn>
+    </MjmlSection>
   );
 }
 
