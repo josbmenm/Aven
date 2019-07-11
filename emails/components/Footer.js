39cae52bb5238aa512f80f6f75bb007a0e5b6d52
Horacio Herrera <me@hherrerag.com>
2019-07-11 02:14:27 +0200

    footer email styles

--- emails/components/Footer.js
+++ emails/components/Footer.js
@@ -1,50 +1,85 @@
 import React from 'react';
 import theme from '../theme';
 
-import { MjmlSection, MjmlColumn, MjmlRaw } from 'mjml-react';
+function FooterText({ children }) {
+  return (
+    <MjmlText
+      align="center"
+      padding="0px"
+      fontSize="7px"
+      lineHeight="9px"
+      color="#9B9B9B"
+    >
+      {children}
+    </MjmlText>
+  );
+}
+
+import {
+  MjmlSection,
+  MjmlColumn,
+  MjmlRaw,
+  MjmlImage,
+  MjmlDivider,
+  MjmlText,
+  MjmlSpacer,
+} from 'mjml-react';
 
 function Footer() {
   return (
-    <MjmlSection
-      backgroundColor="white"
-      padding="0"
-      paddingLeft="136px"
-      paddingRight="136px"
-    >
-      <MjmlColumn padding="28px 36px 20px 36px">
-        <MjmlRaw>
-          <div
-            style={{
-              width: 48,
-              height: 48,
-              backgroundColor: theme.colors.primary,
-            }}
+    <React.Fragment>
+      <MjmlSection
+        borderTop="1px solid #4A4A4A"
+        backgroundColor="white"
+        paddingTop="28px"
+        paddingLeft="136px"
+        paddingRight="136px"
+        paddingBottom="20px"
+      >
+        <MjmlColumn verticalAlign="middle" padding="0px">
+          <MjmlImage
+            padding="0px"
+            src="https://emailassets-kglyyrazbw.now.sh/social_instagram.png"
+            width="28px"
           />
-        </MjmlRaw>
-      </MjmlColumn>
-      <MjmlColumn padding="28px 36px 20px 36px" borderBottom="1px solid hsla(0, 0%, 59%, 0.3)">
-        <MjmlRaw>
-          <div
-            style={{
-              width: 48,
-              height: 48,
-              backgroundColor: theme.colors.primary,
-            }}
+        </MjmlColumn>
+        <MjmlColumn verticalAlign="middle" padding="0px">
+          <MjmlImage
+            padding="0px"
+            src="https://emailassets-kglyyrazbw.now.sh/social_twitter.png"
+            width="28px"
           />
-        </MjmlRaw>
-      </MjmlColumn>
-      <MjmlColumn padding="28px 36px 20px 36px" borderBottom="1px solid hsla(0, 0%, 59%, 0.3)">
-        <MjmlRaw>
-          <div
-            style={{
-              width: 48,
-              height: 48,
-              backgroundColor: theme.colors.primary,
-            }}
+        </MjmlColumn>
+        <MjmlColumn verticalAlign="middle" padding="0px">
+          <MjmlImage
+            padding="0px"
+            src="https://emailassets-kglyyrazbw.now.sh/social_facebook.png"
+            width="28px"
           />
-        </MjmlRaw>
-      </MjmlColumn>
-    </MjmlSection>
+        </MjmlColumn>
+      </MjmlSection>
+
+      <MjmlSection
+        backgroundColor="white"
+        padding="0px"
+        paddingLeft="136px"
+        paddingRight="136px"
+      >
+        <MjmlColumn verticalAlign="middle" padding="0px">
+          <MjmlDivider borderWidth="1px" borderColor="rgba(151,151,151, 0.3)" />
+        </MjmlColumn>
+      </MjmlSection>
+
+      <MjmlSection padding="0px" backgroundColor="white">
+        <MjmlColumn padding="0px 0px 20px 0px">
+          <FooterText>No longer wish to receive these emails?</FooterText>
+          <FooterText>please unsubscribe</FooterText>
+          <MjmlSpacer height="10px" />
+          <FooterText>Ono Food Co.</FooterText>
+          <FooterText>915 Venice Blvd. Los Angeles, CA 90015</FooterText>
+        </MjmlColumn>
+      </MjmlSection>
+    </React.Fragment>
   );
 }
 
