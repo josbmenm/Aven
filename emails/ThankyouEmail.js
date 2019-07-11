beecb8d488ef00e06602b7e703368f5cc7bcfd24
Horacio Herrera <me@hherrerag.com>
2019-07-11 03:19:11 +0200

    move email assets to maui-web

--- emails/ThankyouEmail.js
+++ emails/ThankyouEmail.js
@@ -1,88 +1,80 @@
 import React from 'react';
 import {
   render,
-  Mjml,
-  MjmlHead,
-  MjmlTitle,
-  MjmlPreview,
-  MjmlBody,
+  MjmlRaw,
   MjmlSection,
   MjmlColumn,
   MjmlButton,
+  MjmlImage
 } from 'mjml-react';
+import theme from './theme';
+import Layout from './components/Layout';
 import Text from './components/Text';
-
-function Header({ title, metaTitle }) {
-  return (
-    <MjmlHead>
-      <mj-font
-        name="MaaxBold"
-        href="http://onofood.co/fonts/Maax%20-%20Bold-205TF/Maax%20-%20Bold-205TF.woff2"
-      />
-      <MjmlTitle>{title}</MjmlTitle>
-      <MjmlPreview>{metaTitle}</MjmlPreview>
-    </MjmlHead>
-  );
-}
+import Header from './components/Header';
 
 function getBodyHTML(params) {
   const { html, errors } = render(
-    <Mjml>
+    <Layout>
       <Header />
-      <MjmlBody backgroundColor="#F8F8F8">
-        <MjmlSection
-          paddingLeft="66px"
-          paddingRight="66px"
-          paddingTop="20px"
-          paddingBottom="80px"
-          backgroundColor="white"
-          borderBottom="1px solid #005151"
-        >
-          <MjmlColumn padding="0">
-            <Text>THANK YOU</Text>
-          </MjmlColumn>
-        </MjmlSection>
-        <MjmlSection
-          backgroundColor="white"
-          paddingTop="36px"
-          paddingBottom="36px"
-          paddingLeft="24px"
-          paddingRight="24px"
-        >
-          <MjmlColumn>
-            <Text
-              fontSize="12px"
-              font-Weight="bold"
-              color="#005151"
-              padding="0"
-            >
-              Give a Blend, Get a Blend.
-            </Text>
-            <Text padding="4px 0" fontSize="12px" color="#005151">
-              For every friend who places their first order, you’ll get a free
-              blend on your next visit.
-            </Text>
-          </MjmlColumn>
-          <MjmlColumn>
-            <Text align="right" padding="0" paddingBottom="4px">
-              Share your promo code:
-            </Text>
-            <MjmlButton
-              align="right"
-              padding="0"
-              innerPadding="10px 24px 13px 24px"
-              fontSize="12px"
-              backgroundColor="#eee"
-              fontWeight="bold"
-              color="#005151"
-              borderRadius="3px"
-            >
-              STEPHENK573
-            </MjmlButton>
-          </MjmlColumn>
-        </MjmlSection>
-      </MjmlBody>
-    </Mjml>,
+      <MjmlSection
+      padding="0px"
+        backgroundUrl="https://emailassets-kglyyrazbw.now.sh/blend_thankyou.jpg"
+        backgroundSize="cover"
+        backgroundRepeat="no-repeat"
+      >
+        <MjmlColumn padding="0px">
+          <mj-spacer height="410px" />
+        </MjmlColumn>
+      </MjmlSection>
+      <MjmlSection
+        paddingLeft="66px"
+        paddingRight="66px"
+        backgroundColor="white"
+        borderBottom="1px solid #005151"
+      >
+        <MjmlColumn padding="0">
+          <MjmlImage src="https://emailassets-kglyyrazbw.now.sh/thankyou_title.png"
+                width="205px" paddingTop="32px" paddingBottom="16px" />
+          <Text fontSize="18px" lineHeight="28px" color={theme.colors.primary}>
+          <p>We appreciate the feedback. As a token of our appreciation, we want to give you food a blend for thought. </p>
+          </Text>
+        </MjmlColumn>
+      </MjmlSection>
+      <MjmlSection
+        backgroundColor="white"
+        paddingTop="36px"
+        paddingBottom="36px"
+        paddingLeft="24px"
+        paddingRight="24px"
+      >
+        <MjmlColumn>
+          <Text fontSize="12px" font-Weight="bold" color="#005151" padding="0">
+            Give a Blend, Get a Blend.
+          </Text>
+          <Text padding="4px 0" fontSize="12px" color="#005151">
+            For every friend who places their first order, you’ll get a free
+            blend on your next visit.
+          </Text>
+        </MjmlColumn>
+        <MjmlColumn>
+          <Text align="right" padding="0" paddingBottom="4px">
+            Share your promo code:
+          </Text>
+          <MjmlButton
+            align="right"
+            padding="0"
+            innerPadding="10px 24px 13px 24px"
+            fontSize="12px"
+            backgroundColor="#eee"
+            fontWeight="bold"
+            color="#005151"
+            borderRadius="3px"
+          >
+            STEPHENK573
+          </MjmlButton>
+        </MjmlColumn>
+      </MjmlSection>
+    </Layout>,
     { validationLevel: 'soft' },
   );
 
