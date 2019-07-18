5961b10fd5417707e8269c27ab69fb96330b1979
Horacio Herrera <me@hherrerag.com>
2019-07-18 23:11:10 +0200

    🍑 Bring all web responsive components to dashboard

--- dashboard/Responsive.js
+++ dashboard/Responsive.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTheme } from './Theme';
+import View from '../views/View';
 import CSSProperties from './CSSProperties';
 
 function validateNumberValue(value) {
@@ -8,6 +9,16 @@ function validateNumberValue(value) {
   } else {
     return value;
   }
+}
+
+function useComponentID() {
+  return React.useMemo(() => {
+    return `r-${Math.random()
+      .toString(36)
+      .substring(2, 5)}${Math.random()
+      .toString(36)
+      .substring(2, 5)}`;
+  }, []);
 }
 
 export function Responsive({
@@ -26,13 +37,7 @@ export function Responsive({
   */
   const theme = useTheme();
   const viewports = breakpoints || theme.breakpoints;
-  const compID = React.useMemo(() => {
-    return `r-${Math.random()
-      .toString(36)
-      .substring(2, 5)}${Math.random()
-      .toString(36)
-      .substring(2, 5)}`;
-  }, []);
+  const compID = useComponentID();
 
   function mapStyles(styleIndex = 0) {
     return Object.keys(style)
@@ -68,7 +73,8 @@ export function Responsive({
   return (
     <React.Fragment>
       <style
-        dangerouslySetInnerHTML={{ /* eslint-disable-line */
+        dangerouslySetInnerHTML={{
+          /* eslint-disable-line */
           __html: styleString,
         }}
       />
@@ -80,4 +86,142 @@ export function Responsive({
       )}
     </React.Fragment>
   );
+}
+
+export function ColumnToRow({
+  children,
+  columnReverse = false,
+  rowReverse = false,
+  style = {},
+  breakpoints,
+  resetFlexBasis,
+  ...rest
+}) {
+
+  const elemID = useComponentID();
+  const theme = useTheme();
+  const bp = breakpoints || theme.breakpoints;
+  return (
+    <React.Fragment>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+      .vertical-to-horizontal-layout-${elemID} {
+        display: flex;
+        flex-direction: ${columnReverse ? 'column-reverse' : 'column'};
+      }
+
+      .vertical-to-horizontal-layout-${elemID} > .vertical-to-horizontal-layout__child {
+        margin-left: 0;
+        margin-right: 0;
+      }
+
+      @media only screen and (min-width: ${bp[0]}px) {
+        .vertical-to-horizontal-layout-${elemID} {
+          flex-direction: ${rowReverse ? 'row-reverse' : 'row'};
+        }
+
+        ${
+          !resetFlexBasis
+            ? `.vertical-to-horizontal-layout-${elemID} > .vertical-to-horizontal-layout__child {
+          flex: 1;
+          flex-basis: 0;
+        }`
+            : ''
+        }
+      }
+      `,
+        }}
+      />
+      <View
+        className={`vertical-to-horizontal-layout-${elemID}`}
+        style={style}
+        {...rest}
+      >
+        {children}
+      </View>
+    </React.Fragment>
+  );
+}
+
+export function ColumnToRowChild({
+  className,
+  inverted = false,
+  style,
+  ...rest
+}) {
+  return (
+    // TODO: RESPONSIVE: add/remove margin for responsive
+    <View
+      className={`vertical-to-horizontal-layout__child ${className}`}
+      style={style}
+      {...rest}
+    />
+  );
+}
+
+export function NoFlexToFlex({ children, breakpoints }) {
+  const theme = useTheme();
+  const bp = breakpoints || theme.breakpoints;
+  return (
+    <React.Fragment>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+      .no-flex-to-flex,
+      .no-flex-to-flex > * {
+        flex: none;
+      }
+
+      @media only screen and (min-width: ${bp[0]}px) {
+        .no-flex-to-flex,
+        .no-flex-to-flex > * {
+          flex: 1;
+        }
+      }
+    `,
+        }}
+      />
+      <View className="no-flex-to-flex">{children}</View>
+    </React.Fragment>
+  );
+}
+
+export function ResponsiveDisplay({ breakpoints }) {
+  const theme = useTheme();
+  const bp = breakpoints || theme.breakpoints;
+  return (
+    <style
+      dangerouslySetInnerHTML={{
+        __html: `
+
+        .hide-mobile {
+          display: none;
+        }
+
+        .hide-desktop {
+          display: flex;
+        }
+
+        @media only screen and (min-width: ${bp[0]}px) {
+          .hide-desktop {
+            display: none;
+          }
+
+          .hide-mobile {
+            display: flex;
+          }
+        }
+      `,
+      }}
+    />
+  );
+}
+
+export function HideDesktopView({ className, ...rest }) {
+  return <View className={`hide-desktop ${className}`} {...rest} />;
+}
+
+export function HideMobileView({ className, ...rest }) {
+  return <View className={`hide-mobile ${className}`} {...rest} />;
 }
