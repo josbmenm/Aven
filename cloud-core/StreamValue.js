19ee7ecfc418b722b1e98e5d7095f64895cec7f1
Eric Vicenti <eric@aven.io>
2019-07-23 16:58:23 -0700

    🎭 Prolong the inevitable

--- cloud-core/StreamValue.js
+++ cloud-core/StreamValue.js
@@ -16,7 +16,7 @@ export async function streamLoad(stream, onGetContext) {
   return new Promise((resolve, reject) => {
     let loadTimeout = setTimeout(() => {
       reject(new Error(`Timed out loading "${onGetContext()}".`));
-    }, 10000);
+    }, 30000);
 
     let loadListener = null;
 
