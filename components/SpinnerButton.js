835bd7dc3fd735ee9ed065dbfec97eea08640e6a
Eric Vicenti <eric@aven.io>
2019-09-19 17:00:13 -0700

    minor fix. re deploy

--- components/SpinnerButton.js
+++ components/SpinnerButton.js
@@ -7,11 +7,16 @@ export default function SpinnerButton({
   isLoading,
   children,
   title,
+  disabled,
   ...props
 }) {
   const { colors } = useTheme();
   return (
-    <Button {...props} title={isLoading ? '' : title}>
+    <Button
+      {...props}
+      title={isLoading ? '' : title}
+      disabled={disabled || isLoading}
+    >
       {isLoading && (
         <Spinner color={colors.invertedText} style={{ alignSelf: 'center' }} />
       )}
