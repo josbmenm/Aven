d85ac6fcf3b3a80e4a1ff1b416454604a3556b9b
Cameron Tacklind <cameron@tacklind.com>
2019-11-11 13:20:28 -0800

    🔍Enable source map for errors

--- packages/something-razzle/src/server.js
+++ packages/something-razzle/src/server.js
@@ -3,6 +3,9 @@ import React from 'react';
 import express from 'express';
 import { renderToString } from 'react-dom/server';
 
+// Enable source map support for errors
+import 'source-map-support/register';
+
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const server = express();
