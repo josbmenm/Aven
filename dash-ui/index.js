2c14e54b0d52772027cdca6d51e5564894906491
Horacio Herrera <hi@horacioh.com>
2019-12-18 17:21:24 +0100

    dash-ui index file

---
+++ dash-ui/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from './Button';
+export { default as ButtonLink } from './ButtonLink';
+export { default as Heading } from './Heading';
+export { default as Link } from './Link';
+export { default as MultiSelect } from './MultiSelect';
+export { default as Stack } from './Stack';
+export { default as Text } from './Text';
+export * from './Theme';
