ed081ceec5bac0b9981d4cc121913b80a4b2c2f1
Cameron Tacklind <cameron@tacklind.com>
2020-01-14 14:55:05 -0800

    Make sure we see all output

--- skynet/deploy.sh
+++ skynet/deploy.sh
@@ -8,3 +8,6 @@ ls -l
 
 # Provide current path in the format that GitHub Actions likes
 echo "::set-output name=path::$(pwd)"
+
+echo # Fix bug with GH Actions hiding data
+echo # Fix bug with GH Actions hiding data
