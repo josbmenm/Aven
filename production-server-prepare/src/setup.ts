c2de670502285a15450d23790588a1039f353b04
Cameron Tacklind <cameron@tacklind.com>
2020-01-21 15:09:56 -0800

    Don't let netdata install squawk so much

--- production-server-prepare/src/setup.ts
+++ production-server-prepare/src/setup.ts
@@ -93,13 +93,11 @@ async function setupJournalbeat() {
 
 async function setupNetdata() {
   // Nightly is recommended by Netdata https://docs.netdata.cloud/packaging/installer/#nightly-vs-stable-releases
-  // await spawn('bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait', true),
 
-  // Stable
-  await spawn(
-    'bash <(curl -Ss https://my-netdata.io/kickstart-static64.sh) --dont-wait',
-    true,
-  );
+  // const installScript = 'https://my-netdata.io/kickstart.sh'; // Nightly
+  const installScript = 'https://my-netdata.io/kickstart-static64.sh'; // Stable
+
+  await exec(`bash <(curl -Ss "${installScript}") --dont-wait`);
 
   // TODO: configure to work with nginx?
 }
