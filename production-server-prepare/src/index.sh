bd3ddb360462f2c6407fe33a11a20c38672090bf
Cameron Tacklind <cameron@tacklind.com>
2020-01-21 21:03:51 -0800

    Always update packages if necessary

--- production-server-prepare/src/index.sh
+++ production-server-prepare/src/index.sh
@@ -7,12 +7,12 @@ export NODE=/usr/bin/node
 if [ ! -x "${NODE}" ]; then
     curl -sL https://deb.nodesource.com/setup_12.x | bash -
     apt-get install -y nodejs
-
-    # Install npm dependencies if we decide to start copying a package.json too
-    [ -e package.json ] && npm install --production
 else
     echo "Node already installed"
 fi
+
+# Install npm dependencies if we decide to start copying a package.json too
+[ -e package.json ] && npm ci
 
 echo "Running Node..."
 
