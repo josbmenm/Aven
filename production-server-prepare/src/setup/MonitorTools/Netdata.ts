3617667acea006d9f1c22382b539b63b1f50921c
Cameron Tacklind <cameron@tacklind.com>
2020-01-21 18:39:02 -0800

    Clarification

--- production-server-prepare/src/setup/MonitorTools/Netdata.ts
+++ production-server-prepare/src/setup/MonitorTools/Netdata.ts
@@ -2,11 +2,10 @@ import { exec } from '../../utils/spawn';
 
 export async function setupNetdata() {
   // Nightly is recommended by Netdata https://docs.netdata.cloud/packaging/installer/#nightly-vs-stable-releases
+  const installScriptNightly = 'https://my-netdata.io/kickstart.sh';
+  const installScriptStable = 'https://my-netdata.io/kickstart-static64.sh';
 
-  // const installScript = 'https://my-netdata.io/kickstart.sh'; // Nightly
-  const installScript = 'https://my-netdata.io/kickstart-static64.sh'; // Stable
-
-  await exec(`bash <(curl -Ss "${installScript}") --dont-wait`);
+  await exec(`bash <(curl -Ss "${installScriptStable}") --dont-wait`);
 
   // TODO: configure to work with nginx?
 }
