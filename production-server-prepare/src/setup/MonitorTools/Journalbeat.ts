128b4a1acc15a692af2c531a548fd37f9fa4c7d8
Cameron Tacklind <cameron@tacklind.com>
2020-01-21 18:38:53 -0800

    Add initial journalbeat config

--- production-server-prepare/src/setup/MonitorTools/Journalbeat.ts
+++ production-server-prepare/src/setup/MonitorTools/Journalbeat.ts
@@ -1,6 +1,64 @@
 import { ensureFileIs } from '../../utils/Files';
 import { exec } from '../../utils/spawn';
 
+const journalbeatConfig = `journalbeat.inputs:
+- paths: []
+  #backoff: 1s
+  #max_backoff: 20s
+  seek: cursor
+  #cursor_seek_fallback: head
+  #include_matches: []
+  #fields:
+
+#journalbeat:
+  #registry_file: registry
+
+setup.template.settings:
+  index.number_of_shards: 1
+  #index.codec: best_compression
+  #_source.enabled: false
+
+#name:
+#tags: ["service-X", "web-tier"]
+#fields:
+
+#setup.dashboards.enabled: false
+#setup.dashboards.url:
+
+setup.kibana:
+  host: "bender0.onofood.co:5601"
+
+output.elasticsearch:
+  hosts: ["167.71.113.119:9200"] # TODO: Why is this an IP?
+  #protocol: "https"
+  username: "elastic"
+  password: "logging$ecret" # TODO: Make this actually secret
+
+#output.logstash:
+  #hosts: ["localhost:5044"]
+  #ssl.certificate_authorities: ["/etc/pki/root/ca.pem"]
+  #ssl.certificate: "/etc/pki/client/cert.pem"
+  #ssl.key: "/etc/pki/client/cert.key"
+
+processors:
+  - add_host_metadata: ~
+  - add_cloud_metadata: ~  
+  - decode_json_fields:
+    fields: ["message"]
+    process_array: true
+    max_depth: 8
+    target: ""
+
+#logging.level: debug
+#logging.selectors: ["*"]
+
+#monitoring.enabled: false
+#monitoring.cluster_uuid:
+#monitoring.elasticsearch:
+
+#migration.6_to_7.enabled: true
+`;
+
 export async function setupJournalbeat() {
   await exec(
     'wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -',
@@ -16,6 +74,10 @@ export async function setupJournalbeat() {
   await exec('apt-get install -y journalbeat');
 
   await exec('systemctl enable journalbeat');
+
+  await ensureFileIs('/etc/journalbeat/journalbeat.yml', journalbeatConfig);
+
+  await exec('systemctl start journalbeat');
 
   // TODO: Finish setup
   // https://www.elastic.co/guide/en/beats/journalbeat/current/journalbeat-configuration.html
